9f6af1ec29da23c87233ee7d374205cffa8e4701
ipa-fmw <mmb-weisshardtf@ipa.fhg.de>
2010-05-27 13:34:19 +0200

    modified trajectories

--- cob_dashboard/src/parameters.py
+++ cob_dashboard/src/parameters.py
@@ -117,22 +117,26 @@ class trayParameter:
 class armParameter:
 	action_goal_topic = 'arm_controller/joint_trajectory_action'
 	joint_names = ["arm_1_joint","arm_2_joint","arm_3_joint","arm_4_joint","arm_5_joint","arm_6_joint","arm_7_joint"]
-
 	home = JointTrajectory()
 	home.joint_names = joint_names
 	point=JointTrajectoryPoint()
 	point.positions=[0,0,0,0,0,0,0]
-	point.velocities=[0,0,0,0,0,0,0]
 	point.time_from_start=rospy.Duration(3)
 	home.points.append(point)
 
 	folded = JointTrajectory()
 	folded.joint_names = joint_names
 	point=JointTrajectoryPoint()
-	point.positions=[0,0.7,1.5,1.5,0.7,1.5,0]
-	point.velocities=[0,0,0,0,0,0,0]
+	point.positions=[5.5721,2.0184,4.8944,1.7424,0.1602,0.9800,4.8580]
 	point.time_from_start=rospy.Duration(3)
 	folded.points.append(point)
+	
+	folded2 = JointTrajectory()
+	folded2.joint_names = joint_names
+	point=JointTrajectoryPoint()
+	point.positions=[2.79,-1.74,0,0,0,0,0]
+	point.time_from_start=rospy.Duration(3)
+	folded2.points.append(point)
 
 class lbrParameter:
 	action_goal_topic = 'lbr_controller/joint_trajectory_action'
@@ -296,30 +300,6 @@ class lbrParameter:
 	point.time_from_start=rospy.Duration(12)
 	cupTOtablet.points.append(point)
 	
-class armParameter:
-	action_goal_topic = 'arm_controller/joint_trajectory_action'
-	joint_names = ["arm_1_joint","arm_2_joint","arm_3_joint","arm_4_joint","arm_5_joint","arm_6_joint","arm_7_joint"]
-	home = JointTrajectory()
-	home.joint_names = joint_names
-	point=JointTrajectoryPoint()
-	point.positions=[0,0,0,0,0,0,0]
-	point.time_from_start=rospy.Duration(3)
-	home.points.append(point)
-
-	folded = JointTrajectory()
-	folded.joint_names = joint_names
-	point=JointTrajectoryPoint()
-	point.positions=[5.5721,2.0184,4.8944,1.7424,0.1602,0.9800,4.8580]
-	point.time_from_start=rospy.Duration(3)
-	folded.points.append(point)
-	
-	folded2 = JointTrajectory()
-	folded2.joint_names = joint_names
-	point=JointTrajectoryPoint()
-	point.positions=[2.79,-1.74,0,0,0,0,0]
-	point.time_from_start=rospy.Duration(3)
-	folded2.points.append(point)
-
 class armParameter_pr2:
 	action_goal_topic = 'r_arm_controller/joint_trajectory_action'
 	joint_names = ["r_shoulder_pan_joint","r_shoulder_lift_joint","r_upper_arm_roll_joint","r_elbow_flex_joint","r_forearm_roll_joint","r_wrist_flex_joint","r_wrist_roll_joint"]
