2c3b25dbdc764bc59eb0326e53458ff37f5534e7
abubeck <alexander.bubeck@ipa.fraunhofer.de>
2010-08-06 17:18:45 +0200

    removed tk code

--- cob_dashboard/src/knoeppkes.py
+++ cob_dashboard/src/knoeppkes.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from Tkinter import *
 import roslib
 roslib.load_manifest('cob_dashboard')
 from buttons import *
@@ -19,35 +18,6 @@ def start(func, args):
 
 def startGTK(widget, data):
   data()
-
-class General(Frame):
-  def __init__(self, master=None):
-    width = 5
-    self.charge = StringVar()
-    self.charge.set("0")
-
-    Frame.__init__(self, master, border=2, relief=GROOVE)
-    label = Label(self, text="general", bg="#bfdfff")
-    label.grid(sticky=W+E, padx=0, pady=0, columnspan=2)
-
-    label = Label(self, text="state:")
-    label.grid(sticky=W, padx=3, pady=3, row=1, column=0)
-    charge = Entry(self, width=width, justify=RIGHT,
-        textvariable=self.charge)
-    charge.grid(sticky=W+E, padx=3, pady=3, row=1, column=1)
-    
-    button = Button(self, text="quit", command=quit)
-    button.grid(sticky=W+E, padx=3, pady=3, row=10, column=0)
-
-class Panel(Frame):
-  def __init__(self, master=None, labeltext=""):
-    Frame.__init__(self, master, border=2, relief=GROOVE)
-    label = Label(self, text=labeltext, bg="#bfdfff")
-    label.grid(sticky=W+E, padx=0, pady=0)
-
-  def addButton(self, *args, **keys):
-    button = Button(self, *args, **keys)
-    button.grid(sticky=W+E, padx=3, pady=3)
 
 class GtkGeneralPanel(gtk.Frame):
   def __init__(self):
@@ -94,7 +64,7 @@ class GtkPanel(gtk.Frame):
     self.vbox.pack_start(but, False, False, 5)
         
 
-class Knoeppkes(Frame):
+class Knoeppkes():
   def delete_event(self, widget, event, data=None):
     gtk.main_quit()
     return False
@@ -111,6 +81,8 @@ class Knoeppkes(Frame):
     vbox.pack_start(self.hbox, True, True, 0)
     gpanel = GtkGeneralPanel()
     self.hbox.pack_start(gpanel,True, True, 3)
+    b = buttons()
+    panels = b.panels
     for pname, actions in panels:
       panel = GtkPanel(self, pname)
       for aname, func, args in actions:
@@ -126,22 +98,5 @@ class Knoeppkes(Frame):
     self.window.show_all()
     gtk.gdk.threads_init() 
     gtk.main()
-    # init GUI
-    #Frame.__init__(self)
-    #self.grid()
-    #self.master.title("cob_dashboard")
-    #col = 0
-
-    #panel = General(self)
-    #panel.grid(row=0, column=col, padx=3, pady=3, sticky=N)
-    #col += 1
-
-    #for pname, actions in panels:
-    #  panel = Panel(self, pname)
-    #  for aname, func, args in actions:
-    #    panel.addButton(text=aname, command=lambda f=func, a=args: start(f, a))
-    #  panel.grid(row=0, column=col, padx=3, pady=3, sticky=N)
-    #  col += 1
-
+  
 app = Knoeppkes()
-#app.mainloop()
