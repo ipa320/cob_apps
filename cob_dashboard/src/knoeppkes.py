d34279ea865b7d9c4e82f36c1c68c40f499b2b76
Alexander Bubeck <bubi@bubibook.(none)>
2010-10-21 22:21:31 +0800

    added emergency stop functionality

--- cob_dashboard/src/knoeppkes.py
+++ cob_dashboard/src/knoeppkes.py
@@ -59,13 +59,19 @@
 
 import roslib
 roslib.load_manifest('cob_dashboard')
+from cob_msgs.msg import EmergencyStopState
 from buttons import *
 import thread
 import pygtk
 pygtk.require('2.0')
 import gtk
 import roslib
-import os 
+import os
+import pynotify
+import sys 
+
+#Initializing the gtk's thread engine
+gtk.gdk.threads_init()
 
 ## Executes a button click in a new thread
 def start(func, args):
@@ -79,23 +85,25 @@ def startGTK(widget, data):
 class GtkGeneralPanel(gtk.Frame):
   def __init__(self):
     gtk.Frame.__init__(self)
+    if not pynotify.init ("cob_dashboard"):
+      sys.exit (1)
     self.set_label("general")
     self.set_shadow_type(gtk.SHADOW_IN)
     self.vbox = gtk.VBox(False, 0)
     self.add(self.vbox)
+    #hbox=gtk.HBox(True, 0)
+    #image = gtk.Image()
+    #image.set_from_file(roslib.packages.get_pkg_dir("cob_dashboard") + "/share/icons/batti-040.png")
+    #hbox.pack_start(image, False, False, 0)
+    #label = gtk.Label("40 %")
+    #hbox.pack_start(label, False, False, 0)
+    #self.vbox.pack_start(hbox, False, False, 5)    
     hbox=gtk.HBox(True, 0)
-    image = gtk.Image()
-    image.set_from_file(roslib.packages.get_pkg_dir("cob_dashboard") + "/share/icons/batti-040.png")
-    hbox.pack_start(image, False, False, 0)
-    label = gtk.Label("40 %")
-    hbox.pack_start(label, False, False, 0)
-    self.vbox.pack_start(hbox, False, False, 5)    
-    hbox=gtk.HBox(True, 0)
-    image = gtk.Image()
-    image.set_from_file(roslib.packages.get_pkg_dir("cob_dashboard") + "/share/icons/weather-clear.png")
-    hbox.pack_start(image, False, False, 0)
-    label = gtk.Label("Status OK")
-    hbox.pack_start(label, False, False, 0)
+    self.status_image = gtk.Image()
+    self.status_image.set_from_file(roslib.packages.get_pkg_dir("cob_dashboard") + "/share/icons/weather-clear.png")
+    hbox.pack_start(self.status_image, False, False, 0)
+    self.status_label = gtk.Label("Status OK")
+    hbox.pack_start(self.status_label, False, False, 0)
     self.vbox.pack_start(hbox, False, False, 5)    
 
     #but = gtk.Button("Init all")
@@ -104,7 +112,26 @@ class GtkGeneralPanel(gtk.Frame):
 
     but = gtk.Button(stock=gtk.STOCK_QUIT	)
     but.connect("clicked", lambda w: gtk.main_quit())
-    self.vbox.pack_start(but, False, False, 5)    
+    self.vbox.pack_start(but, False, False, 5)
+
+  def setEMStop(self, em):
+    if(em):
+      #print "Emergency Stop Active"
+      self.status_image.set_from_file(roslib.packages.get_pkg_dir("cob_dashboard") + "/share/icons/weather-storm.png")
+      self.status_label.set_text("EM Stop !")
+      n = pynotify.Notification("Emergency Stop issued!", "", "dialog-warning")
+      icon=gtk.status_icon_new_from_icon_name("important")      
+      n.attach_to_status_icon(icon)
+      n.set_timeout(1000)
+      n.show()
+    else:
+      #print "Status OK"
+      self.status_image.set_from_file(roslib.packages.get_pkg_dir("cob_dashboard") + "/share/icons/weather-clear.png")
+      self.status_label.set_text("Status OK")
+      n = pynotify.Notification("Emergency Stop released!", "", "dialog-ok")
+      n.set_timeout(1000)
+      n.show()
+		    
 
 ## Class for gtk panel implementation
 class GtkPanel(gtk.Frame):
@@ -126,9 +153,14 @@ class Knoeppkes():
   def delete_event(self, widget, event, data=None):
     gtk.main_quit()
     return False
+
+  def emcb(self, msg):
+    self.gpanel.setEMStop(msg.emergency_state)
+    
   def __init__(self):
     # init ros node
     rospy.init_node('cob_knoeppkes')
+    rospy.Subscriber("/emergency_stop_state", EmergencyStopState, self.emcb)
   
     self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
     self.window.connect("delete_event", self.delete_event)
@@ -137,8 +169,8 @@ class Knoeppkes():
     vbox = gtk.VBox(False, 1)
     self.hbox = gtk.HBox(True, 10)
     vbox.pack_start(self.hbox, True, True, 0)
-    gpanel = GtkGeneralPanel()
-    self.hbox.pack_start(gpanel,True, True, 3)
+    self.gpanel = GtkGeneralPanel()
+    self.hbox.pack_start(self.gpanel,True, True, 3)
     b = buttons()
     panels = b.panels
     for pname, actions in panels:
