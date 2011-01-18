32a992ee2cad25a84abc0b30e61c71324083ce41
SimonEbner <Simon.Ebner@gmail.com>
2011-01-18 13:52:44 +0100

    bugfix and stop VNC script

--- cob_webportal/common/files/ros-roscomp.sh
+++ cob_webportal/common/files/ros-roscomp.sh
@@ -1,38 +1,23 @@
 #!/bin/bash
 export ROBOT=cob3-1
-export ROBOT_ENV=brics_rc
+source /opt/ros/cturtle/setup.sh
+#source /home/brics/git/care-o-bot/setup.sh /home/brics/git/care-o-bot
+#source /home/brics/git/cob3_intern/setup.sh /home/brics/git/cob3_intern
 source /opt/ros/cturtle/setup.sh
 source /home/brics/git/care-o-bot/setup.sh /home/brics/git/care-o-bot
-source /home/brics/git/cob3_intern/setup.sh /home/brics/git/cob3_intern
 #source /home/uhr/git/robocup/setup.sh /home/uhr/git/robocup
 
 VIRTUALGL_PATH=/opt/VirtualGL/bin/vglrun
-
+IFS=$'\n'
 
 status() {
-  local package=$name
-  local launchfile=$launchfile
-
-  status=1
-  nodeRunning=0
-  runningNodes=`rosnode list`
-  nodes=`roslaunch --nodes $package $launchfile`
-
-  for node in $nodes; do
-    running=0
-    for rNode in $runningNodes; do
-      if [ $node == $rNode ]; then
-        nodeRunning=1
-      fi
-    done
-
-    if [ $nodeRunning == 0 ]; then
-     status=0
-      break
-    fi
-  done
-
-  echo $status;
+  local searchname=$searchname
+  grep=`ps ax | grep $searchname | grep -v grep | grep -v ros-roscomp.sh`
+  if [[ $grep == '' ]]; then
+    echo 0
+  else
+    echo 1
+  fi
 }
 
 
@@ -69,7 +54,8 @@ if [ $cmd == "stop" -o $cmd == "restart" ]; then
     nodes=`roslaunch --nodes $name $launchfile`
     for node in $nodes; do
       echo " * $node"
-      a=`rosnode kill $node 2>/dev/null`
+      eval "timeout -9 5 rosnode kill $node 1>/dev/null 2>&1"
+#      `timeout -9 5 rosnode kill $node`
     done
 
     sleep 1 # Wait for nodes to shut down
@@ -79,8 +65,10 @@ if [ $cmd == "stop" -o $cmd == "restart" ]; then
   pids=`ps ax | grep $searchname | grep -v grep | grep -v ros-roscomp.sh | sed -e "s/^ *//" | sed -e "s/ .*//"&`
   for pid in $pids; do
     echo " * $pid"
-    `kill -9 $pid 2>/dev/null`
+    `kill -9 $pid 1>/dev/null 2>&1`
   done
+
+  sleep 1
 fi
 
 if [ $cmd = "start" -o $cmd = "restart" ]; then
@@ -93,42 +81,65 @@ if [ $cmd = "start" -o $cmd = "restart" ]; then
 	echo "/opt/ros/cturtle/ros/bin/$rostype $name $launchfile $argStr "
    else
 	echo "$VIRTUALGL_PATH /opt/ros/cturtle/ros/bin/$rostype $name $launchfile $argStr &"
-   	$VIRTUALGL_PATH  /opt/ros/cturtle/ros/bin/$rostype $name $launchfile $argStr &
-   fi
+   	$VIRTUALGL_PATH  /opt/ros/cturtle/ros/bin/$rostype $name $launchfile $argStr  &     
+  fi
 
   echo "Waiting for \"$name\" to be initialized"
-
-  timeout=6
+  
   sleepPerPeriod=1
   time=0
-  running=0
+  timeout=6
   while [ $time -lt $timeout ]; do
     result=$(status "$name" "$launchfile")
-    if [ $result == 1 ]; then
-      running=1
-    fi
 
-    if [ $result == 0 ]; then
+    if [[ $result == 0 ]]; then
       time=$(($time+$sleepPerPeriod))
-      echo "Wait: $sleepPerPeriod, Timeout: $timeout, time: $time"
+      echo "Waiting for all rosnodes to start. Timeout: $timeout, time: $time"
       sleep $sleepPerPeriod
     
     else
       break
     fi
   done
+  echo "* All rosnodes started successfully!"
+
+
 fi
 
 if [ $cmd = "status" ]; then
-  result=$(status "$name" "$launchfile")
+  ps ax | grep $searchname | grep -v grep | grep -v ros-roscomp.sh
 
-  if [ $result == 0 ]; then
-    echo "not_running"
-  elif [ $result == 1 ]; then
-    echo "running"
-  else
-    echo "unexpected_result"
-  fi
+  echo "roslaunch --nodes $name $launchfile"
+
+  runningNodes=`rosnode list`
+  nodes=`roslaunch --nodes $name $launchfile`
+
+  for node in $nodes; do
+    nodeRunning=0
+    echo " * $node"
+
+    # skip $(anon nodes, their names are ramodomly assigned
+    if [[ ${node:0:6} == '$(anon' ]]; then
+       continue
+    fi
+    if [[ $node == 'cob3_gazebo_model' ]]; then
+       continue
+    fi
+
+    for rNode in $runningNodes; do
+      if [ $node == $rNode ]; then
+        nodeRunning=1
+      fi
+    done
+
+    if [[ $nodeRunnin == 1 ]]; then
+       echo "Node Running"
+    else
+      echo "Node NOT Running"
+    fi
+
+  done
+
 
 fi
 
