ff3a519616aaf8a850971bddaa207b26ca96e5bf
ipa-fmw <mmb-weisshardtf@ipa.fhg.de>
2011-02-06 22:40:17 +0100

    added python api test for script server

---
+++ cob_script_server/test/python_api.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python
+import roslib
+roslib.load_manifest('cob_script_server')
+
+import sys
+import unittest
+
+import rospy
+import rostest
+from trajectory_msgs.msg import *
+from simple_script_server import *
+from pr2_controllers_msgs.msg import *
+from cob_srvs.srv import *
+
+class PythonAPITest(unittest.TestCase):
+	def __init__(self, *args):
+		super(PythonAPITest, self).__init__(*args)
+		rospy.init_node('test_python_api_test')
+		self.sss=simple_script_server()
+		self.as_cb_executed = False
+		self.ns_global_prefix = "/script_server"
+
+	def test_python_api(self):
+		# init action server
+		#as_name = "/tray_controller/joint_trajectory_action"
+		#as_name, JointTrajectoryAction
+		#self.as_server = actionlib.SimpleActionServer(as_name, JointTrajectoryAction, execute_cb=self.as_cb)
+
+		# get parameters
+		try:
+			# component
+			if not rospy.has_param('~command'):
+				self.fail('Parameter command does not exist on ROS Parameter Server')
+			command = rospy.get_param('~command')
+		except KeyError, e:
+			self.fail('Parameters not set properly')		
+
+		# call sss functions
+		if command == "move":
+			component_name = "arm"
+			# init action server (as)
+			as_name = "/" + component_name + "_controller/joint_trajectory_action"
+			self.as_server = actionlib.SimpleActionServer(as_name, JointTrajectoryAction, execute_cb=self.as_cb)
+			#execute test
+			self.move_test(component_name,"home")
+			self.move_test(component_name,"folded")
+			self.move_test(component_name,"pregrasp")
+			
+		elif (command == "init" or command == "stop" or command == "recover"):
+			# init stop service server (ss)
+			ss_name = "/script_server/test_stop_service"
+			self.stop_service = rospy.Service(ss_name, Trigger, self.ss_trigger_cb)
+			# execute test
+			self.trigger_test(ss_name)
+			
+		else:
+			self.fail("Command not known to test script")
+
+	def move_test(self,component_name, parameter_name):
+		self.as_cb_executed = False
+		
+		self.sss.move(component_name,parameter_name)		
+		
+		# check result
+		if not self.as_cb_executed:
+			self.fail('No goal received at action server')
+		
+		# get joint_names from parameter server
+		param_string = self.ns_global_prefix + "/" + component_name + "/joint_names"
+		if not rospy.has_param(param_string):
+			error_msg = "parameter " + param_string +" does not exist on ROS Parameter Server"
+			self.fail(error_msg)
+		joint_names = rospy.get_param(param_string)
+		
+		# get joint values from parameter server
+		if type(parameter_name) is str:
+			param_string = self.ns_global_prefix + "/" + component_name + "/" + parameter_name
+			if not rospy.has_param(param_string):
+				error_msg = "parameter " + param_string + " does not exist on ROS Parameter Server"
+			param = rospy.get_param(param_string)
+		else:
+			param = parameter_name
+		
+		traj_msg = JointTrajectory()
+		traj_msg.header.stamp = rospy.Time.now()+rospy.Duration(0.5)
+		traj_msg.joint_names = joint_names
+		point_nr = 0
+		for point in param:
+			point_msg = JointTrajectoryPoint()
+			point_msg.positions = point
+			point_msg.time_from_start=rospy.Duration(3*point_nr) # this value is set to 3 sec per point. \todo: read from parameter
+			traj_msg.points.append(point_msg)
+		
+		#print traj_msg
+		#print self.traj
+		
+		# check amount of trajectory points
+		if not (len(traj_msg.points) == len(self.traj.points)):
+			self.fail('Not the same amount of points in trajectory')
+		
+		# check points
+		for i in range(len(traj_msg.points)):
+			# check dimension of points
+			if not (len(traj_msg.points[i].positions) == len(self.traj.points[i].positions)):
+				self.fail('Not the same amount of values in point')
+			
+			# check values of points
+			for j in range(len(traj_msg.points[i].positions)):
+				if not (traj_msg.points[i].positions[j] == self.traj.points[i].positions[j]):
+					self.fail('Not the same values in point')
+
+	def trigger_test(self,ss_name):
+		try:
+			client = rospy.ServiceProxy(ss_name, Trigger)
+			req = TriggerRequest()
+			resp = client(req)
+		except rospy.ServiceException, e:
+			error_msg = "Service call failed: %s"%e
+			self.fail(error_msg)
+
+	def as_cb(self, goal):
+		result = JointTrajectoryResult()
+		#self.as_server.set_preempted(result)
+		self.as_cb_executed = True
+		self.traj = goal.trajectory
+		print "action server callback"
+		self.as_server.set_succeeded(result)
+	
+	def ss_trigger_cb(self,req):
+		res = TriggerResponse()
+		return res
+
+if __name__ == '__main__':
+	try:
+		rostest.run('rostest', 'test_python_apt_test', PythonAPITest, sys.argv)
+	except KeyboardInterrupt, e:
+		pass
+	print "exiting"
+
