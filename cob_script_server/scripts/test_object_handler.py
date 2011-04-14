380ece1428628fb0d4d2d6e03a94bd5561476dff
ipa-uhr-fm <felix_messmer@web.de>
2011-04-14 15:03:44 +0200

    implemented all object_handler functions

--- cob_script_server/scripts/test_object_handler.py
+++ cob_script_server/scripts/test_object_handler.py
@@ -14,8 +14,10 @@ class Test_Object_Handler(script):
 		if not self.sss.parse:
 			rospy.loginfo("Testing Object_Handler modes...")
 			
-		self.sss.add_object("milk_box")	
-
+		#self.sss.add_object("milk_box")
+		#self.sss.attach_object("milk_box")
+		#self.sss.detach_object("milk_box")
+		self.sss.remove_object("milk_box")
 
 if __name__ == "__main__":
 	SCRIPT = Test_Object_Handler()
