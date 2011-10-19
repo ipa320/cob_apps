514047e3beb9e716d37dc1fba5b6d66af50864ba
ipa-fmw <florian.weisshardt@ipa.fraunhofer.de>
2011-10-19 17:55:35 +0200

    moved webportal to new stack

--- cob_webportal/common/files/runGazebo_virtualgl.sh
+++
@@ -1,6 +0,0 @@
-export ROBOT=cob3-1
-export DISPLAY=:$1
-
-
-
-/opt/VirtualGL/bin/vglrun roslaunch cob_gazebo cob3.launch
--- cob_webportal/common/files/stopVNC.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-echo "Display: $1"
-echo "PID: $2"
-
-/opt/TurboVNC/bin/vncserver -kill :$1
-kill $2
-kill -9 $2
